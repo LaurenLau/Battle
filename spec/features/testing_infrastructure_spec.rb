ac95d6421df91778c3dc7a0f9c28c19feecc9e82
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-07 14:36:49 +0100

    Battle game now has ability to enter user name

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,10 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
+describe 'battle web app', type: :feature do
+
+  it 'will ask players to enter their names' do
     visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+    fill_in 'player_1_name', with: 'Lauren'
+    fill_in 'player_2_name', with: 'Jodi'
+    click_button 'Submit'
+    expect(page).to have_content 'Lauren vs. Jodi'
   end
 end
