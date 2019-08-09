838718dd3504bd8d90df3c834e0909788f76b378
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-09 11:29:57 +0100

    Refactoring

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -2,6 +2,6 @@ feature 'attack player' do
   scenario 'attack player 2' do
     sign_in_and_play
     click_link 'Attack'
-    expect(page).to have_content 'Loren attacked Jodie'
+    expect(page).to have_content 'Player1 attacked Player2'
   end
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,6 +3,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     sign_in_and_play
-    expect(page).to have_content 'Loren vs. Jodie'
+    expect(page).to have_content 'Player1 vs. Player2'
   end
 end
